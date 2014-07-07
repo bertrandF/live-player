9fb3622745f530b4c46506e7627d5718bb528e7b
bertrandF <bertrand.feuillerat@hotmail.fr>
2014-07-07 15:10:03 +0200

    Starts Rtmpdump as child process and pipes its output.

--- main.c
+++ main.c
@@ -39,12 +39,11 @@
 #include <SDL/SDL.h>
 
 #define VERSION_MAJOR   (0)
-#define VERSION_MINOR   (1)
+#define VERSION_MINOR   (2)
 
-#define OPT_STRING      "hd:l:o:r:"
+#define OPT_STRING      "hl:o:r:"
 static struct option long_options[] = {
     {"help",        no_argument,        NULL,   'h'},
-    {"device",      required_argument,  NULL,   'd'},
     {"loops",       required_argument,  NULL,   'l'},
     {"output",      required_argument,  NULL,   'o'},
     {"framerate",   required_argument,  NULL,   'r'},
@@ -53,14 +52,13 @@ static struct option long_options[] = {
 
 #define WINDOW_NAME         "WEBCAM"
 
-#define DEFAULT_DEVICE      (1)
 #define DEFAULT_FRAMERATE   (10)
 #define DEFAULT_FRAMEWIDTH  (352)
 #define DEFAULT_FRAMEHEIGHT (288)
 #define DEFAULT_LOOPS       (50)
 #define DEFAULT_OUTFCC      (CV_FOURCC('M', 'P', '4', 'V'))
+#define DEFAULT_INFILE      "/dev/stdin"
 
-static int  device      = DEFAULT_DEVICE;
 static int  frameRate   = DEFAULT_FRAMERATE;
 static struct {
     int width;
@@ -69,19 +67,18 @@ static struct {
 static int  loops       = DEFAULT_LOOPS;
 static char *outfile    = NULL;
 static int  sleepTime   = 1000000 / DEFAULT_FRAMERATE;
-static char *infile     = NULL;
+static char *url        = NULL;
 
 void
 usage(char* name)
 {
     fprintf(stdout, "%s version %d.%d\n", name, VERSION_MAJOR, VERSION_MINOR);
     fprintf(stdout, "Reads video from input file and display it in a SDL window.\n");
-    fprintf(stdout, "Usage: %s [options] <input_file>\n", name);
+    fprintf(stdout, "Usage: %s [options] <url>\n", name);
     fprintf(stdout, "\n");
     fprintf(stdout, "Options:\n");
     fprintf(stdout, "  -h, --help            Prints this help\n");
     fprintf(stdout, "\n");
-    fprintf(stdout, "  -d, --device <n>      Index of video device to use (defaults to %d)\n", DEFAULT_DEVICE);
     fprintf(stdout, "  -l, --loops <n>       Number of loops (defaults to %d)\n", DEFAULT_LOOPS);
     fprintf(stdout, "  -o, --output <file>   Save output to file\n");
     fprintf(stdout, "  -r, --framerate <n>   Set framerate (defaults to %d)\n", DEFAULT_FRAMERATE);
@@ -93,6 +90,7 @@ main(int argc, char** argv )
 {
     char opt;
     int i, videoStreamIdx=-1;
+    int pid, pipefd[2];
     uint32_t start;
     int end = 0;
     
@@ -114,11 +112,8 @@ main(int argc, char** argv )
     while( (opt=getopt_long(argc, argv, OPT_STRING, long_options, NULL))>0 ) {
         switch(opt)  {
             case 'h':
-                usage(argv[1]);
+                usage(argv[0]);
                 return 0;
-                break;
-            case 'd':
-                loops = atoi(optarg);
                 break;
             case 'l':
                 loops = atoi(optarg);
@@ -136,26 +131,44 @@ main(int argc, char** argv )
     }
     // Get input file
     if(optind>0 && optind<argc) {
-         if( strcmp(argv[optind], "-")==0 ) {
-            if( (infile=(char*)malloc(16))==NULL ) {
-                fprintf(stderr, "malloc(): error\n");
-                return -1;
-            }
-            strcpy(infile, "/dev/stdin");
-         } else {
-            infile = argv[optind];
-         }
+        url = argv[optind];
     } else {
-        fprintf(stderr, "Please specify input file.\n");
+        fprintf(stderr, "Please specify streaming URL.\n");
         return -1;
     }
     
     // Dump configuration
-    fprintf(stdout, "inputfile=%s\n",   infile);
     fprintf(stdout, "framerate=%d\n",   frameRate);
     fprintf(stdout, "loops=%d\n",       loops);
     fprintf(stdout, "sleeptime=%d\n",   sleepTime);
-    fprintf(stdout, "device=%d\n",      device);
+    fprintf(stdout, "URL=%s\n",         url);
+
+    
+    // --------------------- Start RTMP dump child process ----------
+    if(pipe(pipefd) != 0) {
+        fprintf(stderr, "Cannot create pipe.\n");
+        return -1;
+    }
+
+    pid=fork();
+    switch(pid) {
+        case -1: // error
+            fprintf(stderr, "Could not create child process for rtmpdump.\n");
+            return -1;
+        case 0: // child
+            close(pipefd[0]); // read end unused
+            close(STDOUT_FILENO);
+            dup(pipefd[1]); // pipe write end is now stdout
+            close(pipefd[1]);
+            execl("/usr/bin/rtmpdump", "--quiet", "--live", "--realtime", "-r", url, (char*)NULL);
+            break;
+        default: // parent
+            close(pipefd[1]); // write end unused
+            close(STDIN_FILENO);
+            dup(pipefd[0]); // pipe read end is now stdin
+            close(pipefd[0]);
+            break;
+    }
 
 
     // --------------------- Configure lib AV ------------------------
@@ -167,7 +180,7 @@ main(int argc, char** argv )
     }
     
     // Open input file
-    if( avformat_open_input(&fmtCtx, infile, 0, NULL)!=0 ) {
+    if( avformat_open_input(&fmtCtx, DEFAULT_INFILE, 0, NULL)!=0 ) {
         fprintf(stderr, "Cannot open input file.\n");
         return -1;
     }
@@ -177,7 +190,7 @@ main(int argc, char** argv )
         fprintf(stderr, "Cannot find stream info.\n");
         return -1;
     }
-    av_dump_format(fmtCtx, 0, infile, 0);
+    av_dump_format(fmtCtx, 0, DEFAULT_INFILE, 0);
 
     // Find the first video stream
     for(i=0 ; i<fmtCtx->nb_streams ; ++i) {
@@ -230,6 +243,7 @@ main(int argc, char** argv )
         return -1;
     }
 
+    
     // --------------------- Process video frames -------------------
     rect.x = 0;
     rect.y = 0;
@@ -283,7 +297,7 @@ main(int argc, char** argv )
                 pict.linesize[1] = bmp->pitches[2];
                 pict.linesize[2] = bmp->pitches[1];
 
-                av_picture_copy(&pict, frame, frame->format, codecCtx->width,
+                av_picture_copy(&pict, (AVPicture*)frame, frame->format, codecCtx->width,
                         codecCtx->height);
 
                 SDL_UnlockYUVOverlay(bmp);
